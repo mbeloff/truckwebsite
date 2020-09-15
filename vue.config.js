1fd0032475bcb2308df28550fe19173c98b9f4cd
Michael Beloff <38679121+mbeloff@users.noreply.github.com>
2020-09-15 12:31:46 +1000

    vue config file

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Budget Truck Rental - Cheapest Truck Hires in Brisbane and Sydney - No Truck Licence Requied'
+    }
+  }
+}
