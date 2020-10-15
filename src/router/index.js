deeb6bc993607ae74bc7316f63f5f62292bd22d0
michael beloff <michael.a.beloff@gmail.com>
2020-10-15 13:33:39 +1000

    fix robots

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,59 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-const Home = () => import("../views/Home.vue");
-const Success = () => import("../views/Success.vue");
-const NotFound = () => import("../views/NotFound.vue");
-const Contact = () => import("../views/Contact.vue");
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+const Home = () => import('../views/Home.vue')
+const Success = () => import('../views/Success.vue')
+const NotFound = () => import('../views/NotFound.vue')
+const Contact = () => import('../views/Contact.vue')
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: "/success",
-    name: "Success",
+    path: '/success',
+    name: 'Success',
     component: Success
   },
   {
-    path: "/contact",
-    name: "Contact",
+    path: '/contact',
+    name: 'Contact',
     component: Contact
   },
   {
-    path: "/404",
-    name: "404",
+    path: '/404/',
+    name: '404',
     component: NotFound
   },
   {
-    path: "/*",
+    path: '/*',
     component: NotFound
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition;
+      return savedPosition
     } else {
-      return { x: 0, y: 0 };
+      return { x: 0, y: 0 }
     }
   }
-});
+})
 
-export default router;
+export default router
